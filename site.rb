fe06408fd663a94a2381e763f757a259a72de193
Jason L Perry <jasper@ambethia.com>
2015-05-14 12:23:10 -0400

    Added yaml data loading

--- site.rb
+++ site.rb
@@ -1,5 +1,6 @@
 require 'erb'
 require 'webrick'
+require 'yaml'
 
 ROOT = File.dirname(__FILE__)
 
@@ -8,6 +9,7 @@ server = WEBrick::HTTPServer.new(:Port => 8000)
 server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
 
 server.mount_proc '/' do |req, res|
+  @data = YAML.load_file('data.yml')
   template = ERB.new(File.read("#{ROOT}/index.html.erb"))
   res.body = template.result
 end
