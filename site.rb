04049b6f9a5dd3c240df3419614d2bc490ffbf4c
Jason L Perry <jasper@ambethia.com>
2015-05-14 11:21:07 -0400

    Asset handling

--- site.rb
+++ site.rb
@@ -3,9 +3,11 @@ require 'webrick'
 
 ROOT = File.dirname(__FILE__)
 
-server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => "#{ROOT}/public")
+server = WEBrick::HTTPServer.new(:Port => 8000)
 
-server.mount_proc '/ice_cream' do |req, res|
+server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
+
+server.mount_proc '/' do |req, res|
   template = ERB.new(File.read("#{ROOT}/index.html.erb"))
   res.body = template.result
 end
