cd23e3427cf838e99b0d4a37889b4d8d121ad4f6
Jason L Perry <jasper@ambethia.com>
2015-05-18 11:35:08 -0400

    Add slim

--- site.rb
+++ site.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'tilt'
 require 'erb'
 require 'webrick'
 require 'yaml'
@@ -10,8 +13,8 @@ server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
 
 server.mount_proc '/' do |req, res|
   @data = YAML.load_file('data.yml')
-  template = ERB.new(File.read("#{ROOT}/index.html.erb"))
-  res.body = template.result
+  template = Tilt.new("#{ROOT}/index.slim")
+  res.body = template.render
 end
 
 trap 'INT' do
