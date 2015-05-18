468622eafdf42757ae4cd0a0a36517740ebf2cea
Aaron Harvey <harvnlenny@gmail.com>
2015-05-18 12:08:38 -0400

    started to convert to slim

--- site.rb
+++ site.rb
@@ -7,14 +7,16 @@ require 'yaml'
 
 ROOT = File.dirname(__FILE__)
 
-server = WEBrick::HTTPServer.new(:Port => ENV["PORT"])
+PORT = ENV["PORT"] || 8000
+
+server = WEBrick::HTTPServer.new(:Port => PORT)
 
 server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
 
 server.mount_proc '/' do |req, res|
-  @data = YAML.load_file('data.yml')
+  data = YAML.load_file('data.yml')
   template = Tilt.new("#{ROOT}/index.slim")
-  res.body = template.render
+  res.body = template.render({:data => data})
 end
 
 trap 'INT' do
